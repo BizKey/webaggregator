2e7c0e4d0316b439d2bbd6ca48addded1058655e
kizianov.ao <kizianov.ao@svrauto.ru>
2025-08-25 13:42:55 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use actix_web::{App, HttpServer, web};
-use dotenv::dotenv;
 
 mod handlers;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok();
 
     println!("Sever run on http://0.0.0.0:8080");
 
