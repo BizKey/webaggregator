0f40d79450ab89cfd392fc862f060c2ef41e2909
kizianov.ao <kizianov.ao@svrauto.ru>
2026-05-13 14:03:51 +0000

    .

--- src/handlers/pg.rs
+++ src/handlers/pg.rs
@@ -28,7 +28,7 @@ pub async fn pg(pool: web::Data<PgPool>) -> Result<HttpResponse> {
         }
     };
 
-    let pg_stats_table_info = match sqlx::query_as::<_, PgTableInfo>(
+    let pg_stats_table_info: Vec<PgTableInfo> = match sqlx::query_as::<_, PgTableInfo>(
         "
         SELECT schemaname, relname, seq_scan, seq_tup_read, idx_scan, idx_tup_fetch, n_tup_ins, n_tup_upd, n_tup_del, n_live_tup, n_dead_tup
         FROM pg_stat_user_tables;
@@ -43,7 +43,7 @@ pub async fn pg(pool: web::Data<PgPool>) -> Result<HttpResponse> {
         }
     };
 
-    match sqlx::query_as::<_, PgTableIndex>(
+    let pg_stats_table_index: Vec<PgTableIndex> = match sqlx::query_as::<_, PgTableIndex>(
         "
         SELECT schemaname, relname, idx_scan, idx_tup_read, idx_tup_fetch
         FROM pg_stat_user_indexes;
@@ -52,50 +52,52 @@ pub async fn pg(pool: web::Data<PgPool>) -> Result<HttpResponse> {
     .fetch_all(pool.get_ref())
     .await
     {
-        Ok(pg_stats_table_index) => {
-            let pg_stat_statements = sqlx::query_as::<_, PgStatStatements>(
-        "SELECT query, calls, total_exec_time, mean_exec_time, rows FROM pg_stat_statements ORDER BY total_exec_time DESC LIMIT 100;",
-    )
-    .fetch_all(pool.get_ref())
-    .await
-    .map_err(|e| {
-        eprintln!("Database error: {}", e);
-        actix_web::error::ErrorInternalServerError("Database error")
-    })?;
-
-            let pg_stat_table_size  = match sqlx::query_as::<_, PgStatTableSize>(
-        "
-        SELECT schemaname, relname, pg_size_pretty(pg_total_relation_size(schemaname || '.' || relname)) AS total_size, pg_size_pretty(pg_relation_size(schemaname || '.' || relname)) AS table_size, pg_size_pretty(pg_indexes_size(schemaname || '.' || relname)) AS indexes_size
-        FROM pg_stat_user_tables;
-        ",
-    )
-    .fetch_all(pool.get_ref())
-    .await {
-        Ok(pg_stat_table_size ) => pg_stat_table_size ,
+        Ok(pg_stats_table_index) => pg_stats_table_index,
         Err(e) => {
             eprintln!("Database error: {}", e);
-        return Err(actix_web::error::ErrorInternalServerError("Database error"))
+            return Err(actix_web::error::ErrorInternalServerError("Database error"));
         }
     };
 
-            let template = PgTemplate {
-                pg_stats_connections: pg_stats_connections,
-                pg_stats_table_info: pg_stats_table_info,
-                pg_stats_table_index: pg_stats_table_index,
-                pg_stat_statements: pg_stat_statements,
-                pg_stat_table_size: pg_stat_table_size,
-                elapsed_ms: start.elapsed().as_millis(),
-            };
-            match template.render() {
-                Ok(html) => Ok(HttpResponse::Ok()
-                    .content_type("text/html; charset=utf-8")
-                    .body(html)),
-                Err(_) => Ok(HttpResponse::InternalServerError().body("Error template render")),
+    let pg_stat_statements = match sqlx::query_as::<_, PgStatStatements>(
+        "SELECT query, calls, total_exec_time, mean_exec_time, rows FROM pg_stat_statements ORDER BY total_exec_time DESC LIMIT 100;",
+        )
+        .fetch_all(pool.get_ref())
+        .await {
+            Ok(pg_stat_statements) => pg_stat_statements,
+            Err(e) => {
+                  eprintln!("Database error: {}", e);
+            return Err(actix_web::error::ErrorInternalServerError("Database error"));
             }
-        }
-        Err(e) => {
-            eprintln!("Database error: {}", e);
-            Ok(actix_web::error::ErrorInternalServerError("Database error").into())
-        }
+        };
+
+    let pg_stat_table_size  = match sqlx::query_as::<_, PgStatTableSize>(
+            "
+            SELECT schemaname, relname, pg_size_pretty(pg_total_relation_size(schemaname || '.' || relname)) AS total_size, pg_size_pretty(pg_relation_size(schemaname || '.' || relname)) AS table_size, pg_size_pretty(pg_indexes_size(schemaname || '.' || relname)) AS indexes_size
+            FROM pg_stat_user_tables;
+            ",
+        )
+        .fetch_all(pool.get_ref())
+        .await {
+            Ok(pg_stat_table_size ) => pg_stat_table_size,
+            Err(e) => {
+                eprintln!("Database error: {}", e);
+                return Err(actix_web::error::ErrorInternalServerError("Database error"))
+            }
+        };
+
+    let template = PgTemplate {
+        pg_stats_connections: pg_stats_connections,
+        pg_stats_table_info: pg_stats_table_info,
+        pg_stats_table_index: pg_stats_table_index,
+        pg_stat_statements: pg_stat_statements,
+        pg_stat_table_size: pg_stat_table_size,
+        elapsed_ms: start.elapsed().as_millis(),
+    };
+    match template.render() {
+        Ok(html) => Ok(HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(html)),
+        Err(_) => Ok(HttpResponse::InternalServerError().body("Error template render")),
     }
 }
