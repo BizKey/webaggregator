11befca8164413a40c465471eb04bbadecab91f7
kizianov.ao <kizianov.ao@svrauto.ru>
2026-07-20 13:48:40 +0000

    .

--- src/handlers/balance.rs
+++ src/handlers/balance.rs
@@ -1,41 +1,39 @@
-use crate::api::models::Balance;
-use crate::api::templates::BalanceTemplate;
-use actix_web::{HttpResponse, Result, web};
+use actix_web::{HttpResponse, Result as ActixResult, web};
 use askama::Template;
-
 use sqlx::PgPool;
 use std::time::Instant;
 
-pub async fn balances(pool: web::Data<PgPool>) -> Result<HttpResponse> {
-    // balances
+use crate::api::models::Balance;
+use crate::api::templates::BalanceTemplate;
 
-    // time start
+pub async fn balances(pool: web::Data<PgPool>) -> ActixResult<HttpResponse> {
     let start: Instant = Instant::now();
 
-    let balances: Vec<Balance> =  match sqlx::query_as::<_, Balance>("SELECT exchange, account_id, available, available_change, currency, hold_value, hold_change, relation_event, relation_event_id, event_time, total, symbol, order_id, trade_id, updated_at FROM balance ORDER BY updated_at DESC LIMIT 1000;")
-        .fetch_all(pool.get_ref())
-        .await {
-            Ok(balances) => balances,
-            Err(e) => {
-                let msg: String = format!("Database error: {}", e);
-                log::error!("{}", msg);
-                return  Ok(actix_web::error::ErrorInternalServerError("Database error").into())
-            }
-        };
+    let balances: Vec<Balance> = sqlx::query_as::<_, Balance>(
+        r#"
+        SELECT exchange, account_id, available, available_change, currency, hold_value, hold_change, relation_event, relation_event_id, event_time, total, symbol, order_id, trade_id, updated_at
+        FROM balance
+        ORDER BY updated_at DESC
+        LIMIT 1000;
+        "#)
+        .fetch_all(pool.as_ref())
+        .await
+        .map_err(|e| {
+            log::error!("Database error in balances handler: {}", e);
+            actix_web::error::ErrorInternalServerError("Database error")
+        })?;
 
     let template: BalanceTemplate = BalanceTemplate {
         balances: balances,
         elapsed_ms: start.elapsed().as_millis(),
     };
 
-    let html: String = match template.render() {
-        Ok(html) => html,
-        Err(_) => return Ok(HttpResponse::InternalServerError().body("Error template render")),
-    };
+    let html: String = template.render().map_err(|e| {
+        log::error!("Template render error: {}", e);
+        actix_web::error::ErrorInternalServerError("Error template render")
+    })?;
 
-    let response: HttpResponse = HttpResponse::Ok()
+    Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(html);
-
-    Ok(response)
+        .body(html))
 }
